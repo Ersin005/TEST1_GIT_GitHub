35db188834cd9060d4107f6a9c2e5a850b2816f6
Ersin KAYHAN <dekked2438@gmail.com>
2022-09-30 21:22:15 +0300

    My second Commit _recommitting

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Created GIT repository");
+        System.out.println("I shared my project on GitHub");
+        System.out.println("Started working on my project");
     }
 }
